6a8d8eb48cc8224bde20657ea095417f3dcf91db
Etienne Kornobis <ekornobis@gmail.com>
2020-12-14 16:14:30 +0100

    refactoring of the DebugJob class

--- sequana_pipetools/slurm.py
+++ sequana_pipetools/slurm.py
@@ -17,40 +17,25 @@ class DebugJob:
         self.path = Path(path)
         self.slurm_out = sorted([f for f in self.path.glob("slurm*.out")])
 
-        self._parse_snakemaster()
+        with open(self.slurm_out[0], "r") as f:
+            self.snakemaster = f.read()
+
+        self.percent = self._get_percent()
+        self.rules_with_error, self.error_log_files = self._get_rules_with_errors()
+        self.n_errors = len(self.rules_with_error)
 
     def __repr__(self):
 
+        return self._report()
+
+    def _report(self):
+
         message = "#" * 33 + " DEBUG REPORT " + "#" * 33 + "\n\n"
-        message += f"A total of {self.n_errors} errors have been found.\n"
-        message += f"Rules with errors are: {', '.join(self.rules_with_error)}\n\n"
-        message += "Following are the corresponding logs:\n\n"
-        message += f"{self.error_log}"
+        message += f"The analysis reached {self.percent}%. A total of {self.n_errors} errors has been found.\n\n"
 
-        return message
-
-    def _parse_snakemaster(self):
-        """Parse the main snakemake output file"""
-
-        step_percent = "{:d} of {:d} steps ({percent:g}%) done"
-        errors = """Error in rule {rule:S}:
-    jobid: {jobid:d}
-    output: {output:S}
-    log: {log:S} (check log file(s) for error message)
-    """
-
-        with open(self.slurm_out[0], "r") as snakemaster:
-
-            f = snakemaster.read()
-            # Get last percentage
-            last_percent_parse = [x for x in parse.findall(step_percent, f)][-1]
-
-            self.percent = last_percent_parse["percent"]
-
-            self.rules_with_error = [e["rule"] for e in parse.findall(errors, f)]
-            self.error_log_files = [
-                self.path / e["log"] for e in parse.findall(errors, f)
-            ]
+        if self.n_errors:
+            message += f"Rules with errors are: {', '.join(self.rules_with_error)}\n\n"
+            message += "Following are the corresponding logs:\n\n"
 
             self.error_log = ""
             for log in self.error_log_files:
@@ -60,4 +45,32 @@ class DebugJob:
                     self.error_log += l.read()
                     self.error_log += "=" * 80 + "\n\n"
 
-            self.n_errors = len(self.rules_with_error)
+            message += f"{self.error_log}\n"
+
+        message += "\n" + "#" * 80
+        return message
+
+    def _get_percent(self):
+        """Get at which percentage the analysis stopped"""
+
+        step_percent = "{:d} of {:d} steps ({percent:g}%) done"
+
+        # Get last percentage
+        last_percent_parse = [x for x in parse.findall(step_percent, self.snakemaster)]
+        return last_percent_parse[-1]["percent"]
+
+    def _get_rules_with_errors(self):
+        """Return name and log files of rules which returned an error.,"""
+
+        errors = """Error in rule {rule:S}:
+    jobid: {jobid:d}
+    output: {output:S}
+    log: {log:S} (check log file(s) for error message)
+    """
+
+        rules_with_error = [e["rule"] for e in parse.findall(errors, self.snakemaster)]
+        error_log_files = [
+            self.path / e["log"] for e in parse.findall(errors, self.snakemaster)
+        ]
+
+        return rules_with_error, error_log_files
