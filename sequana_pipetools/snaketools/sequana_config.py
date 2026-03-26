8e145cb00d4367b1ec902fe43cdba2b8c0186b52
Thomas Cokelaer <cokelaer@gmail.com>
2026-03-26 14:43:23 +0100

    Add __getitem__ to _Namespace for dict-like key access

--- sequana_pipetools/snaketools/sequana_config.py
+++ sequana_pipetools/snaketools/sequana_config.py
@@ -37,6 +37,9 @@ class _Namespace(SimpleNamespace):
     def __contains__(self, key):
         return hasattr(self, key)
 
+    def __getitem__(self, key):
+        return getattr(self, key)
+
 
 try:
     import importlib.resources as resources
